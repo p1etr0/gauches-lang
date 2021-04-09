8747ad77c7d1f277b42e6c531b93ba5d9e7a3aa1
Pietro de Souza Gonçalves <pietro_goncalves@hotmail.com>
2021-04-09 15:04:47 -0300

    V1

--- fonte/exemplos/Gauches.java
+++ fonte/exemplos/Gauches.java
@@ -1,5 +1,29 @@
+import java.io.File;
+import java.util.Scanner;
+
+
 public class Gauches {
-    public static void main(String[] args) {
-        System.out.println("Hello world !!!");
-    }
+public static void main(String[] args) {
+    try {
+        String[] lines = new String[2000];
+        int amount = 0;
+        File arquivo = new File("teste.txt");
+        Scanner entrada = new Scanner(arquivo);
+        
+        while (entrada.hasNextLine()) {
+            String line = entrada.nextLine();
+            lines[amount++] = line;
+        }
+
+        entrada.close();
+        
+        for (int i = 0; i < amount; i++) {
+            System.out.println("Linha " + i + ": " + lines[i]);
+        }
+        } catch (Exception e) {
+            System.out.println("Nao foi possivel abrir o arquivo teste.txt");
+            System.out.println("Ele existe mesmo?");
+            e.printStackTrace();
+        }
+}
 }
