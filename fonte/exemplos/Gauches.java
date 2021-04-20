08ff8548081d401c8183db28613bda2047796e0c
[p1etr0] <pietro_goncalves@hotmail.com>
2021-04-19 21:13:09 -0300

    Ajustado a validação e armazenamento de variaveis e seus valores

--- fonte/exemplos/Gauches.java
+++ fonte/exemplos/Gauches.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.io.File;
 import java.io.FileReader;
 import java.util.Scanner;
@@ -5,6 +6,7 @@ import java.util.Scanner;
 public class Gauches {
 public static void main(String[] args) {
   try {
+      //Abre e le o arquivo e armazena em um array:
       String[] linhas = new String[2000];
       int contador = 0;
       File file = new File("teste.txt");
@@ -14,15 +16,14 @@ public static void main(String[] args) {
           String line = entrada.nextLine();
           linhas[contador++] = line;
       }
-
       entrada.close();
+
       
-       //Jutando o arquivo em uma linha: 
+        //Jutando o arquivo em uma linha: 
       String linha = "";
       for (int i = 0; i < linhas.length ; i++){
           if(linhas[i] != null){
-            linha = linha.concat(linhas[i]); 
-            
+            linha = linha.concat(linhas[i]);  
           }
       }
 
@@ -33,11 +34,11 @@ public static void main(String[] args) {
       }
 
       // Dividindo o arquivo em um array quebrando no (" "):
-      //o Array frases tem cada palavra do arquivo:
+      // O Array frases[] tem cada palavra do arquivo:
       String palavra = "";
       String frases[] = new String[2000];
       String frasesCertas[][] = new String[2000][2000];
-      for(int i = 0; i < 3; i++){
+      for(int i = 0; i < arrayFrases.length; i++){
         palavra = arrayFrases[i];
         frases = palavra.split(" ");
        for (int j = 0; j < frases.length; j++){
@@ -45,32 +46,51 @@ public static void main(String[] args) {
        }
       }
 
-      //I == linha // J == palavra
+      //Cada array foi criado para um tipo de variavel:
+      ArrayList<String> memoriaTodo = new ArrayList<String>();
+      ArrayList<String> memoriaQuebrado = new ArrayList<String>();
+      ArrayList<String> memoriaJaguarice = new ArrayList<String>();
+      ArrayList<String> memoriaGarrancho = new ArrayList<String>();
+      
+      //I == linha || J == palavra
       for(int i = 0; i < frasesCertas.length; i++){
           for(int j = 0; j < frasesCertas.length; j++){
+            //Recebe cada palavra do arquivo
             palavra = frasesCertas[i][j];
-            switch(palavra) {
-              case "Todo":
-              System.out.println("entrei");
-              String var = frasesCertas[i][j+1];
-              String valor = frasesCertas[i][j+3];
-              Sintaxe.armazenaVetor(var,valor);
-              System.exit(0);
-                break;
-              case "Quebrado":
-                // code block
-                break;
-              default:
-                System.out.println("Palavra não encontrada na linguagem Gauches (Olhe o Manual.md)");
-                break;
+            if (palavra != null){
+              //Faz a verificação se a palavra é uma variavel e armazena no array indicado:
+              switch(palavra) {
+                case "Todo":
+                  String varT = frasesCertas[i][j+1];
+                  String valorT = frasesCertas[i][j+3];
+                  memoriaTodo.add(varT);
+                  memoriaTodo.add(valorT);
+                  break;
+                case "Quebrado":
+                  String varQ = frasesCertas[i][j+1];
+                  String valorQ = frasesCertas[i][j+3];
+                  memoriaQuebrado.add(varQ);
+                  memoriaQuebrado.add(valorQ);
+                  break;
+                case "Jaguarice":
+                  String varJ = frasesCertas[i][j+1];
+                  String valorJ = frasesCertas[i][j+3];
+                  memoriaJaguarice.add(varJ);
+                  memoriaJaguarice.add(valorJ);
+                  break;
+                case "Garrancho":
+                  String varG = frasesCertas[i][j+1];
+                  String valorG = frasesCertas[i][j+3];
+                  memoriaGarrancho.add(varG);
+                  memoriaGarrancho.add(valorG);
+                  break;
+                default:
+                  break;
+              }
             }
           }
       }
       
-      
-
-
-
     
     } 
     catch (Exception e) {
--- fonte/exemplos/Sintaxe.java
+++ fonte/exemplos/Sintaxe.java
@@ -1,14 +1,15 @@
 public class Sintaxe {
-    
+    //ainda não esta sendo utilizado pra nada (NÃO FUNCIONA)
     public static void armazenaVetor(String var, String valor){
         String memoria[][] = new String[11][11];
         for (int i = 0; i < memoria.length; i++){
-            for(int j = 0; j <= i ; j++){
-                j = i;
+            int parametro=0;
+            for(int j = 0; parametro <= i; j++){
                 memoria[i][j] = var;
                 memoria[i][j+1] = valor;
-                System.out.println("Variavel " + memoria[0][0] + " " + "Valor " + memoria[0][1]);
+                System.out.println("Variavel " + memoria[i][j] + " " + "Valor " + memoria[i][j+1]);
+                parametro = i+1;
             }
-        }
+        } 
     }    
 }
